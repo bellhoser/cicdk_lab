155193d1bf2b99e0b1e71963dc5d18766fd4db7a
Peter Bellows <peter.bellows@gmail.com>
2023-12-01 17:10:18 -0500

    Third commit

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as pipelines from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './demoawspipeline-app-stack'
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 
 
 
@@ -21,5 +23,12 @@ export class DemoawspipelineStack extends cdk.Stack {
         input: pipelines.CodePipelineSource.gitHub('bellhoser/cicdk_lab', 'main', ),
         commands: [ 'npm ci', 'npm run build', 'npx cdk synth']})});
       
+    const testingStage = democicdpipeline.addStage(new PipelineAppStage(this, 'test', {
+      env: { account: '733210860975', region: 'us-east-1' }}));
+    
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    const prodStage = democicdpipeline.addStage(new PipelineAppStage(this, 'prod', {
+      env: { account: '733210860975', region: 'us-east-1' }}));
   }
 }
